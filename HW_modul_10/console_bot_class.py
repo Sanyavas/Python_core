17c0ebfa40549255de829e5fc808ac3c2bf95f91
Sanyavas <vasilinaoleksandr@gmail.com>
2022-12-26 15:12:58 +0200

    add change

--- HW_modul_10/console_bot_class.py
+++ HW_modul_10/console_bot_class.py
@@ -24,8 +24,9 @@ class Record:
     def add_phone(self, phones):
         self.phones.append(phones)
 
-    def change_phone(self, phones):
-        self.phones.remove(phones)
+    def change_phone(self, phone_old, phone_new):
+        self.phones.remove(phone_old)
+        self.phones.append(phone_new)
 
     def remove_contact(self, phones):
         self.phones.remove(phones)
